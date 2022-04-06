1c2e099b4c215643ea74c1fa6b3552424b5b6204
Ari <chan.ari96@gmail.com>
2022-04-06 16:11:08 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    }
+    else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    else {
+        let x, y, sum = 0;
+        x = (num1 > num2) ? num1 : num2;
+        y = (num1 > num2) ? num2 : num1;
 
+        for (let i = y; i <= x; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
