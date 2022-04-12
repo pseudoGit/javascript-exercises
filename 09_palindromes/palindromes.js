ca3cb9e94ae939330d70d58f9fb8827b9c1724d0
Ari <chan.ari96@gmail.com>
2022-04-12 12:59:24 -0400

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let result = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"").toLowerCase();
+    return result === result.split("").reverse().join("");
 };
 
 // Do not edit below this line
