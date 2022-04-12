12fdbdf16445e9c2c34d62594d75d4491e3718bf
Ari <chan.ari96@gmail.com>
2022-04-12 13:09:47 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0) {
+        return "OOPS";
+    }
+    let fib0 = 0;
+    let fib1 = 1;
+    for (let i = 1; i < a; i++){
+        let temp = fib1;
+        fib1 = fib1 + fib0;
+        fib0 = temp;
+    }
+    return fib1;
 };
 
 // Do not edit below this line
