50b4ea9cc3693687fa1e3a8d1f4cc15944e4a233
Ari <chan.ari96@gmail.com>
2022-04-12 13:41:56 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort(function(a, b) {
+        const currentYear = (new Date()).getFullYear();
+        if (!('yearOfDeath' in a)) {
+            a.yearOfDeath = currentYear;
+        }
+        if (!('yearOfDeath' in b)) {
+            b.yearOfDeath = currentYear;
+        }
+        const current = a.yearOfDeath - a.yearOfBirth;
+        const next = b.yearOfDeath - b.yearOfBirth;
+        return (current > next) ? -1 : 1;
+    });
+    console.log(oldest);
+    return oldest[0];
 };
 
 // Do not edit below this line
